e9d58d4b9eebafae723455f8abfc5f46b26feef3
HannahBenz <65340536+HannahBenz@users.noreply.github.com>
2020-11-19 16:20:17 +0100

    Update sphere.py
    Earth, Sun and Moon have i texture now

--- sphere.py
+++ sphere.py
@@ -12,20 +12,32 @@
 #############################################################
 
 import vtk
+jpegSonne = "sun.jpg"
+jpegErde = "earth.jpg"
+jpegMond = "moon.jpg"
 
 class Himmelskoerper():
     def __init__(self, Umkreisungskoerper, position, radius, color):
 
-        self.Port = vtk.vtkSphereSource()
-        self.Port.Umkreisungskoerper = Umkreisungskoerper
-        self.Port.SetCenter(position)
-        self.Port.SetRadius(radius)
-        self.Port.color = color
-        self.Port.SetPhiResolution(100)
-        self.Port.SetThetaResolution(100)
+        self.Source = vtk.vtkSphereSource()
+        self.Source.Umkreisungskoerper = Umkreisungskoerper
+        self.Source.Position = self.Source.SetCenter(position)
+        self.Source.SetRadius(radius)
+        self.Source.color = color
+        self.Source.SetPhiResolution(100)
+        self.Source.SetThetaResolution(100)
 
 
+refresh_rate = 60 # In Hertz
+def callback_func(caller, timer_event):
 
+    # This needs to be called to render the updated actor
+    # orientation.
+    window.Render()
+
+#############################################################
+# Create the objects that should be rendered
+# Create a sphere using the vtk class.
 
 Sonne = Himmelskoerper(0, (0, 0, 0), 5, "yellow")
 Erde = Himmelskoerper(Sonne, (8, -5, 0), 1, "blue")
@@ -37,53 +49,138 @@ colors = vtk.vtkNamedColors()
 
 
 #############################################################
-
+'''transform = vtk.vtkTransform()
+transform.Translate()
+transformFilter = vtk.vtkTransformPolyDataFilter()
+transformFilter.SetTransform(transform)
+transformFilter.SetInputConnection(Erde.Source.GetOutputPort())
+transformFilter.SetInputConnection(Mond.Source.GetOutputPort())
+transformFilter.Update()'''
 #############################################################
 # fill the VTK rendering pipeline with the data of the objects
 # source (-> filter) -> mapper -> actor -> renderer -> render window
-mapper = vtk.vtkPolyDataMapper()
-mapper2 = vtk.vtkPolyDataMapper()
-mapper3 = vtk.vtkPolyDataMapper()
-mapper.SetInputConnection(Sonne.Port.GetOutputPort())
-mapper2.SetInputConnection(Erde.Port.GetOutputPort())
-mapper3.SetInputConnection(Mond.Port.GetOutputPort())
+#reader
+readerSonne = vtk.vtkJPEGReader()
+readerSonne.SetFileName(jpegSonne)
+readerErde = vtk.vtkJPEGReader()
+readerErde.SetFileName(jpegErde)
+readerMond = vtk.vtkJPEGReader()
+readerMond.SetFileName(jpegMond)
 
-actor = vtk.vtkActor()
-actor2 = vtk.vtkActor()
-actor3 = vtk.vtkActor()
-actor.SetMapper(mapper)
-actor2.SetMapper(mapper2)
-actor3.SetMapper(mapper3)
-actor.GetProperty().SetColor(colors.GetColor3d("Yellow"))
-actor2.GetProperty().SetColor(colors.GetColor3d("Blue"))
-actor3.GetProperty().SetColor(colors.GetColor3d("Red"))
+#texture
+#Sonne
+textureSonne = vtk.vtkTexture()
+if vtk.VTK_MAJOR_VERSION <= 5:
+    textureSonne.setInput(readerSonne.GetOutput())
+else:
+    textureSonne.SetInputConnection(readerSonne.GetOutputPort())
 
+#Erde
+textureErde = vtk.vtkTexture()
+if vtk.VTK_MAJOR_VERSION <= 5:
+    textureErde.setInput(readerErde.GetOutput())
+else:
+    textureErde.SetInputConnection(readerErde.GetOutputPort())
+
+#Mond
+textureMond = vtk.vtkTexture()
+if vtk.VTK_MAJOR_VERSION <= 5:
+    textureMond.setInput(readerMond.GetOutput())
+else:
+    textureMond.SetInputConnection(readerMond.GetOutputPort())
+
+# Map texture coordinates
+#Sonne
+map_to_sphere_Sonne = vtk.vtkTextureMapToSphere()
+if vtk.VTK_MAJOR_VERSION <= 5:
+    map_to_sphere_Sonne.SetInput(Sonne.Source.GetOutput())
+else:
+    map_to_sphere_Sonne.SetInputConnection(Sonne.Source.GetOutputPort())
+map_to_sphere_Sonne.PreventSeamOn()
+
+#Erde
+map_to_sphere_Erde = vtk.vtkTextureMapToSphere()
+if vtk.VTK_MAJOR_VERSION <= 5:
+    map_to_sphere_Erde.SetInput(Erde.Source.GetOutput())
+else:
+    map_to_sphere_Erde.SetInputConnection(Erde.Source.GetOutputPort())
+map_to_sphere_Erde.PreventSeamOn()
+
+#Mond
+map_to_sphere_Mond = vtk.vtkTextureMapToSphere()
+if vtk.VTK_MAJOR_VERSION <= 5:
+    map_to_sphere_Mond.SetInput(Mond.Source.GetOutput())
+else:
+    map_to_sphere_Mond.SetInputConnection(Mond.Source.GetOutputPort())
+map_to_sphere_Mond.PreventSeamOn()
+
+#mapper
+#Sonne
+mapperSonne = vtk.vtkPolyDataMapper()
+
+if vtk.VTK_MAJOR_VERSION <= 5:
+    mapperSonne.SetInput(map_to_sphere_Sonne.GetOutput())
+else:
+    mapperSonne.SetInputConnection(map_to_sphere_Sonne.GetOutputPort())
+
+#Erde
+mapperErde = vtk.vtkPolyDataMapper()
+
+if vtk.VTK_MAJOR_VERSION <= 5:
+    mapperErde.SetInput(map_to_sphere_Erde.GetOutput())
+else:
+    mapperErde.SetInputConnection(map_to_sphere_Erde.GetOutputPort())
+
+#Mond    
+mapperMond = vtk.vtkPolyDataMapper()
+
+if vtk.VTK_MAJOR_VERSION <= 5:
+    mapperMond.SetInput(map_to_sphere_Mond.GetOutput())
+else:
+    mapperMond.SetInputConnection(map_to_sphere_Mond.GetOutputPort())
+
+#actor
+actorSonne = vtk.vtkActor()
+actorErde = vtk.vtkActor()
+actorMond = vtk.vtkActor()
+actorSonne.SetMapper(mapperSonne)
+actorSonne.SetTexture(textureSonne)
+actorErde.SetMapper(mapperErde)
+actorErde.SetTexture(textureErde)
+actorMond.SetMapper(mapperMond)
+actorMond.SetTexture(textureMond)
+
+#############################################################
+##Render
 renderer = vtk.vtkRenderer()
-render_window = vtk.vtkRenderWindow()
-render_window.SetWindowName("Sphere")
-render_window.AddRenderer(renderer)
-render_window_interactor = vtk.vtkRenderWindowInteractor()
-render_window_interactor.SetRenderWindow(render_window)
-
-renderer.AddActor(actor)
-renderer.AddActor(actor2)
-renderer.AddActor(actor3)
+renderer.AddActor(actorSonne)
+renderer.AddActor(actorErde)
+renderer.AddActor(actorMond)
 renderer.SetBackground(colors.GetColor3d("Black"))
+##window
+window = vtk.vtkRenderWindow()
+window.SetWindowName("Universe")
+window.AddRenderer(renderer)
+interactor = vtk.vtkRenderWindowInteractor()
+interactor.SetRenderWindow(window)
+# The interactor needs to be initialised before we
+# can create a timer or add observers.
+interactor.Initialize()
+# The repeating timer takes the interval in 1/1000th
+# of a second. Each time it fires off a TimerEvent.
+interactor.CreateRepeatingTimer(int(1/refresh_rate))
+# In python you can add an observer directly like this.
+# In other languages there is a layer of indireciton where
+# a vtkCallbackCommand is created, and the function is set
+# in the vtkCallbackCommand.
+# We set the callback function to be called for each
+# activation of the timer.
+interactor.AddObserver("TimerEvent", callback_func)
+# The default position of the camera
+# is a little too close to see the entire
+# motion of the arrow. Zoom out a litle.
+cam = renderer.GetActiveCamera()
+cam.SetPosition(0,0,50)
 
-render_window.Render()
-render_window_interactor.Start()
-    #############################################################
-    # Aufgabe 3
-    #transform = vtk.vtkTransform()
-    #transform.Translate(3, 2, 1)
-    #transform.SetInputConnection(Mond.GetOutputPort())
-    #transform.Update()
-    # Sign up to receive TimerEvent
-    #cb = vtk.TimerCallback()
-    #cb.actor = actor
-    #vtk.renderWindowInteractor.AddObserver('TimerEvent', cb.execute)
-    #timerId = vtk.renderWindowInteractor.CreateRepeatingTimer(100)
-
-    #start the interaction and timer
-    #vtk.renderWindowInteractor.Start()'''
-
+window.Render()
+interactor.Start()
